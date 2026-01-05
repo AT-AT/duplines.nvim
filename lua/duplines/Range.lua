3e29f5e9d99c913d687853352a60bbefd91d71d9
AT-AT <dec.alpha21264@gmail.com>
2026-01-05 11:01:49 +0900

    refactor: move key sequence generator

--- lua/duplines/Range.lua
+++ lua/duplines/Range.lua
@@ -9,7 +9,6 @@
 ---@class Range
 ---@field from_pos fun(): Range Factory (class method)
 ---@field col_index fun(): index_tuple
----@field key_sequence fun(self, CursorPos): string
 ---@field pos_on_begin fun(): index_tuple
 ---@field pos_on_end fun(): index_tuple
 ---@field pos_on_head fun(): index_tuple
@@ -17,18 +16,9 @@
 ---@field row_count fun(): integer
 ---@field row_index fun(): index_tuple
 ---@field to_next fun(): Range
----@field to_key_sequence fun(self, from: index_tuple, to: index_tuple): string
 ---@field col range_index
 ---@field row range_index
 ---@field inverted boolean
-
-
--- =================================================================================================
---  Loaded Module
--- =================================================================================================
-
----@module 'duplines.enum'
-local const = require('duplines.enum')
 
 
 -- =================================================================================================
@@ -36,7 +26,6 @@ local const = require('duplines.enum')
 -- =================================================================================================
 
 local Range = {}
-local L = {}
 
 
 -- =================================================================================================
@@ -52,26 +41,6 @@ function Range:new (o)
   setmetatable(o, self)
   self.__index = self
   return o
-end
-
--- / Helper
--- -------------------------------------------------------------------------------------------------
-
-table.unpack = table.unpack or unpack ---@diagnostic disable-line:deprecated
-
----@param from integer
----@param to integer
----@return string
-function L.to_col_keyseq(from, to)
-  local delta = to - from
-
-  if delta == 0 then
-    return ''
-  elseif delta > 0 then
-    return delta .. 'l'
-  else
-    return math.abs(delta) .. 'h'
-  end
 end
 
 -- / Class Method
@@ -112,48 +81,6 @@ end
 
 function Range:is_inverted()
   return self.inverted
-end
-
-function Range:key_sequence(cursor_pos)
-  local begin_idx = {}
-  local end_idx = {}
-
-  if cursor_pos == const.CURSOR_POS.head then
-    begin_idx = self:pos_on_tail()
-    end_idx = self:pos_on_head()
-  elseif cursor_pos == const.CURSOR_POS.tail then
-    begin_idx = self:pos_on_head()
-    end_idx = self:pos_on_tail()
-  else
-    begin_idx = self:pos_on_begin()
-    end_idx = self:pos_on_end()
-  end
-
-  local from_row, from_col = table.unpack(begin_idx)
-  local to_row, to_col = table.unpack(end_idx)
-
-  -- Single row.
-  if from_row == to_row then
-    return L.to_col_keyseq(from_col, to_col)
-  end
-
-  -- Vertical direction.
-  local row_keyseq = ''
-  local delta = to_row - from_row
-  if delta > 0 then
-    row_keyseq = delta .. 'j'
-  else
-    row_keyseq = math.abs(delta) .. 'k'
-  end
-
-  -- If col can be moved as is to the destination row, the amount of horizontal movement will simply
-  -- be the difference.
-  if to_col >= from_col then
-    return row_keyseq .. L.to_col_keyseq(from_col, to_col)
-  end
-
-  -- Otherwise, the length of the destination row is unknown, so move via the left edge.
-  return L.to_col_keyseq(from_col, 0) .. row_keyseq .. L.to_col_keyseq(0, to_col)
 end
 
 function Range:pos_on_head()
